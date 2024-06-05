dd7ee2df43986528e3c17840746768fc9a336ac3
Dawid Piwko <dpiwko@gmail.com>
2024-06-05 23:20:22 +0200

    [#2] - dpiwko - printBuffer singleton - (2423)

---
+++ Singleton/PrintBuffer/Client.cs
@@ -0,0 +1,19 @@
+﻿namespace DesignPatterns.Singleton.PrintBuffer;
+
+public class Client
+{
+  // public static void Main()
+  // {
+  //   var printBuffer = PrintBuffer.Instance;
+
+  //   printBuffer.AddToBuffer("print element");
+  //   printBuffer.AddToBuffer("new element to print");
+  //   printBuffer.AddToBuffer("print it");
+  //   printBuffer.PrintContent();
+  // }
+}
+
+//// OUTPUT:
+//// print element
+//// new element to print
+//// print it
---
+++ Singleton/PrintBuffer/PrintBuffer.cs
@@ -0,0 +1,36 @@
+﻿namespace DesignPatterns.Singleton.PrintBuffer;
+
+public class PrintBuffer
+{
+  private static PrintBuffer? _instance;
+
+  private readonly Queue<string> _buffer = new Queue<string>();
+
+  private PrintBuffer() { }
+
+  public static PrintBuffer Instance
+  {
+    get
+    {
+      if (_instance is null)
+      {
+        _instance = new PrintBuffer();
+      }
+
+      return _instance;
+    }
+  }
+
+  public void AddToBuffer(string content)
+  {
+    _buffer.Enqueue(content);
+  }
+
+  public void PrintContent()
+  {
+    while (_buffer.Count > 0)
+    {
+      Console.WriteLine(_buffer.Dequeue());
+    }
+  }
+}
